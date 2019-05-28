88b0997ae2edf9839db058da28d05fa8170b519c
Hudson8811 <hudson88@yandex.ru>
2019-05-28 10:43:23 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,26 @@
+$('.slider').owlCarousel({
+    loop:true,
+    margin:20,
+	nav:true,
+	navText:false,
+	dots:true,
+	responsive:{
+		0:{
+            items:1,
+            nav:true
+        },
+		500:{
+            items:1,
+            nav:true
+        },
+		
+		800:{
+            items:2,
+            nav:true
+        },
+		1200:{
+            items:2,
+            nav:true
+        }
+    }
+});
